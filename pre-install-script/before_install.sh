7ecdab535f632639b7f73077d1913b840004b834
Pranav Chaudhari <125885447+techdecipher@users.noreply.github.com>
2024-07-24 11:59:07 +0530

    Install httpd automatically

--- pre-install-script/before_install.sh
+++ pre-install-script/before_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#update the system
+yum update -y
+
+# Install Apache HTTP Server
+yum install httpd -y
+
+# Start and enable httpd
+systemctl start httpd
+systemctl enable httpd
 # Remove the existing index.html file
 if [ -f /var/www/html/index.html ]; then
     rm /var/www/html/index.html
