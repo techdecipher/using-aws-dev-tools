c4b3ae9cac2a347d0417ac7c8525f1d8f0f369f9
techecipher <waytopranav22@gmail.com>
2024-07-25 04:40:21 +0000

    update script to install httpd auto

--- pre-install-script/before_install.sh
+++ pre-install-script/before_install.sh
@@ -8,8 +8,5 @@ yum install httpd -y
 # Start and enable httpd
 systemctl start httpd
 systemctl enable httpd
-# Remove the existing index.html file
-if [ -f /var/www/html/index.html ]; then
-    rm /var/www/html/index.html
-fi
+
 
