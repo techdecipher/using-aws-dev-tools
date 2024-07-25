fe5db608e82d36ddbdb26c4215ab567ad5d08423
Pranav Chaudhari <125885447+techdecipher@users.noreply.github.com>
2024-07-25 13:54:59 +0530

    Update before_install.sh

--- pre-install-script/before_install.sh
+++ pre-install-script/before_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#update the system
-yum update -y
-
-# Install Apache HTTP Server
-yum install httpd -y
-
-# Start and enable httpd
-systemctl start httpd
-systemctl enable httpd
+if [ -f /var/www/html/index.html ]; then
+    rm /var/www/html/index.html
+fi
 
 
